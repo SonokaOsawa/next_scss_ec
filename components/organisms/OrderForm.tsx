de72d4b372183702eff43cf8946be4e47ef11ca7
SonokaOsawa <sooonokaaa@gmail.com>
2022-05-17 17:37:48 +0900

    エラー修正

--- components/organisms/OrderForm.tsx
+++ components/organisms/OrderForm.tsx
@@ -282,7 +282,6 @@ export const OrderForm: FC = () => {
                       </label>
                       <select
                         id="time"
-                        name="time"
                         {...register("time", { required: true })}
                         autoComplete="time"
                         className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -310,7 +309,6 @@ export const OrderForm: FC = () => {
                       </label>
                       <select
                         id="paymethod"
-                        name="paymethod"
                         {...register("paymethod", { required: true })}
                         autoComplete="paymethod"
                         className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
