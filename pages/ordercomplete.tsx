42280498b6c6420ec97d5224a2029e01bac75341
SonokaOsawa <sooonokaaa@gmail.com>
2022-05-17 17:34:49 +0900

    注文完了画面

---
+++ pages/ordercomplete.tsx
@@ -0,0 +1,11 @@
+const OrderComp = () => {
+  return (
+    <>
+      <h2>決済が完了しました！</h2>
+      <p>この度はご注文ありがとうございます。</p>
+      <p>「注文履歴」からご確認ください。</p>
+    </>
+  );
+};
+
+export default OrderComp;
